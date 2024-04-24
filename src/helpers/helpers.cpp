99e7a15c2891dd40bf429c07d6000b7dc2dd965a
Jelligeth <jelligeth@gmail.com>
2024-04-24 14:12:43 +0200

    updates keyValueFind function

--- src/helpers/helpers.cpp
+++ src/helpers/helpers.cpp
@@ -6,25 +6,22 @@
 /*   By: dliu <dliu@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/23 19:01:24 by dliu          #+#    #+#                 */
-/*   Updated: 2024/04/23 19:05:35 by dliu          ########   odam.nl         */
+/*   Updated: 2024/04/24 13:50:02 by dliu          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "helpers.hpp"
+#include "iostream"
 
 /**
  * Finds and returns the substr in string found after the "key", terminated / deliminated by 'delim'
 */
 std::string Helpers::_keyValueFind(std::string string, std::string key, char delim)
 {
-	size_t	start;
-	size_t	end;
-
-	start = string.find(key);
+	size_t start = string.find(key);
 	if (start == std::string::npos)
 		return ("");
 	start += key.length();
-	start += key.find_first_not_of(delim, start);
-	end = key.find_first_of(delim, start);
-	return(key.substr(start, end - start));
+	size_t end = string.find_first_of(delim, start);
+	return(string.substr(start, end - start));
 }
