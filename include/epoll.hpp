5bc6e080a61d037f4c864eabfe19514f30c592be
root <root@LAPTOP-8F9EHJKE>
2024-05-08 08:48:26 +0200

    start

--- include/epoll.hpp
+++ include/epoll.hpp
@@ -1,6 +1,24 @@
 //epoll stuff goes here
 
+#ifndef EPOLL_HPP
+#define EPOLL_HPP
+
+# include <sys/epoll.h>
+
 class Epoll
 {
+	public:
+		Epoll(Socket &socket);//create epoll instance
+		~Epoll();
 
+		//methods need to create
+		int initEpoll(); //create epoll, if error return ERROR
+		int initEpollEvents();//if error return ERROR
+		int initConnection();//if error return ERROR
+		int addSocket();//if error return ERROR
+		int waitEpoll();//if error return ERROR
+	private:
+		Epoll();
 };
+
+#endif
--- src/epoll/epoll.cpp
+++ src/epoll/epoll.cpp
@@ -2,3 +2,45 @@
  * Epoll stuff goes here.
  * @todo remove setup epoll from server class and create new Epoll class with public methods to add fds to the instance etc.
 */
+
+#include "Epoll.hpp"
+
+Epoll::Epoll()
+{
+
+}
+
+Epoll::Epoll(Socket &socket)
+{
+
+}
+
+Epoll::~Epoll()
+{
+
+}
+
+Epoll::initEpoll()
+{
+
+}
+
+Epoll::initEpollEvents()
+{
+
+}
+
+Epoll::initConnection()
+{
+
+}
+
+Epoll::addSocket()
+{
+
+}
+
+Epoll::waitEpoll()
+{
+
+}
