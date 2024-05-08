60c0e7d7c4a60f2530eec66bf9f810e0f772d90b
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2024-05-08 16:53:45 +0200

    done with epoll add mod del

--- include/epoll.hpp
+++ include/epoll.hpp
@@ -13,12 +13,12 @@
 class Epoll
 {
 	public:
-		Epoll();						//create epoll instance
-		~Epoll();						//close epollfd
+		Epoll();										//create epoll instance
+		~Epoll();										//close epollfd
 
-		void addFd(int fd); 			//addition of sockets
-		void modifyFd(int fd);			//modification of sockets
-		void removeFd(int fd);			//removal of sockets
+		void addFd(int fd, uint32_t events); 			//addition of sockets
+		void modifyFd(int fd, uint32_t events);			//modification of sockets
+		void removeFd(int fd);							//removal of sockets
 		std::vector<epoll_event> wait_events(int max_events = CLI_LIMIT, int timeout = -1);
 	private:
 		int _epollfd;
@@ -26,7 +26,10 @@ class Epoll
 
 /* 
  concept:
- 1. create an epoll instance using epoll_create1();
- 2. 
+ 1. Create an epoll instance using epoll_create1();
+ 2. Populate an epoll_event structure and ass it to the epoll instance 
+ with epoll_ctl(), specifying whether to add, modify, or remove file descriptors and their event masks.
+ 3. Use epoll_wait() to wait for events on the epoll instance. This function will return
+ a list of epoll_event structure for file descriptors that have their specified events pending
 */
 #endif
--- src/epoll/epoll.cpp
+++ src/epoll/epoll.cpp
@@ -3,11 +3,11 @@
  * @todo remove setup epoll from server class and create new Epoll class with public methods to add fds to the instance etc.
 */
 
-//epoll_events is a data type used in Linux programming.
-//It's used to describe which events the application is interested in 
-//and any associated user data with the file descriptors that are being monitored by epoll.
-
 /* 
+epoll_events is a data type used in Linux programming.
+It's used to describe which events the application is interested in 
+and any associated user data with the file descriptors that are being monitored by epoll.
+
 struct epoll_event {
     uint32_t events;      Epoll events
     epoll_data_t data;    User data variable 
@@ -30,19 +30,34 @@ Epoll::~Epoll()
     close(_epollfd);
 }
 
-void Epoll::addFd(int fd)
+void Epoll::addFd(int fd, uint32_t events)
 {
-    struct epoll_event event {};
+    struct epoll_event event{};
+    event.events = events;
+    event.data.fd = fd;
+    if (epoll_ctl(_epollfd, EPOLL_CTL_ADD, fd, &event))
+    {
+        throw (WebservException("Failed to add file descriptor in epoll"));
+    }
 }
 
-void Epoll::modifyFd(int fd)
+void Epoll::modifyFd(int fd, uint32_t events)
 {
-
+    struct epoll_event event{};
+    event.events = events;
+    event.data.fd = fd;
+    if (epoll_ctl(_epollfd, EPOLL_CTL_MOD, fd, &event) == -1)
+    {
+        throw (WebservException("Failed to modify file descriptor in epoll"));
+    }
 }
 
 void Epoll::removeFd(int fd)
 {
-
+    if (epoll_ctl(_epollfd, EPOLL_CTL_DEL, fd, nullptr) == -1)
+    {
+        throw (WebservException("Failed to remove file descriptor in epoll"));
+    }
 }
 
 std::vector<epoll_event> Epoll::wait_events(int max_events = CLI_LIMIT, int timeout = -1)
